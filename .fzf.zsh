0fce4819824c19bbb17d8c31d970f4cb566cf289
Vincent Zhou <fireflysuccess@gmail.com>
2020-03-11 11:17:27 +0800

    Update local fzf
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,33 +1,13 @@
 # Setup fzf
 # ---------
-if [[ -d "/usr/local/opt/fzf/" ]]; then
-    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-      export PATH="$PATH:/usr/local/opt/fzf/bin"
-    fi
-    # Auto-completion
-    # ---------------
-    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-    # Key bindings
-    # ------------
-    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+if [[ ! "$PATH" == */home/pengyu/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/pengyu/.fzf/bin"
 fi
-
-if [[ -d "$HOME/.fzf/" ]]; then
-    if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-      export PATH="$PATH:$HOME/.fzf/bin"
-    fi
 
 # Auto-completion
 # ---------------
-    [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/pengyu/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-    source "$HOME/.fzf/shell/key-bindings.zsh"
-fi
-
-if [ -d /usr/share/fzf/ ]; then
-    source /usr/share/fzf/key-bindings.zsh
-    source /usr/share/fzf/completion.zsh
-fi
+source "/home/pengyu/.fzf/shell/key-bindings.zsh"
