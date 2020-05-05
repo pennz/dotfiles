0bd60a315c6932c3f27872fd5c38e46b0bbf123f
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-05 13:51:58 +0800

    fzf revert back config
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,33 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/v/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/v/.fzf/bin"
+if [[ -d "/usr/local/opt/fzf/" ]]; then
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+      export PATH="$PATH:/usr/local/opt/fzf/bin"
+    fi
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 fi
+
+if [[ -d "$HOME/.fzf/" ]]; then
+    if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+      export PATH="$PATH:$HOME/.fzf/bin"
+    fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/v/.fzf/shell/completion.zsh" 2> /dev/null
+    [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/v/.fzf/shell/key-bindings.zsh"
+    source "$HOME/.fzf/shell/key-bindings.zsh"
+fi
+
+if [ -d /usr/share/fzf/ ]; then
+    source /usr/share/fzf/key-bindings.zsh
+    source /usr/share/fzf/completion.zsh
+fi
